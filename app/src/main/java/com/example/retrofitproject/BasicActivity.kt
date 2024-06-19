c18f27332310cba8c03f7022f8ba923a49ca3f04
Mikhail8787 <mikhail.belkov.96@mail.ru>
2024-06-19 20:19:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/retrofitproject/BasicActivity.kt
@@ -0,0 +1,20 @@
+package com.example.retrofitproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.retrofitproject.databinding.ContentBasicBinding
+
+class BasicActivity : AppCompatActivity() {
+    private lateinit var binding: ContentBasicBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ContentBasicBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/retrofitproject/retrofit/MainApi.kt
@@ -0,0 +1,31 @@
+package com.example.retrofitproject.retrofit
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MainApi {
+    @GET("auth/products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
+
+    @POST("auth/login")
+    suspend fun auth(@Body authRequest: AuthRequest): User
+
+    @GET("auth/products")
+    suspend fun getsAllProducts(): Products
+
+    @Headers("Content-Type : application/json")
+    @GET("auth/products/search")
+    suspend fun getsProductsByNameAuth(@Header("Authorization") token: String,
+                                   @Query("q") name: String): Products
+
+
+
+
+
+
+}
